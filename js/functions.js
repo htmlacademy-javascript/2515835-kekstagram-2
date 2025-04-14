a589d902357103fd6e2655292febdfb01586127a
Tuvakova <tuvakovakamilla54@gmail.com>
2025-04-14 14:13:32 +0300

    Исправила ошибки

--- js/functions.js
+++ js/functions.js
@@ -1,11 +1,11 @@
-function isLengthValid(inputString, maxLength) {
-  return inputString.length <= maxLength;
+function isLengthValid(inputString, maxLen) {
+  return inputString.length <= maxLen;
 }
-const stringToCheck = "Привет, мир!";
+const stringToCheck = 'Привет, мир!';
 const maxLength = 15;
 
 if (isLengthValid(stringToCheck, maxLength)) {
-  console.log("Строка допустима.");
+  // console.log('Строка допустима.');//
 } else {
-  console.log("Строка слишком длинная.");
+  // console.log('Строка слишком длинная.');//
 }
--- js/photos.js
+++ js/photos.js
@@ -2,17 +2,17 @@ import { generateComments } from './comments.js';
 import { getRandomInt } from './utils.js';
 
 export function generatePhotos() {
-    const photos = [];
+  const photos = [];
 
-    for (let i = 1; i <= 25; i++) {
-        photos.push({
-            id: i,
-            url: `photos/${i}.jpg`,
-            description: `Описание фотографии номер ${i}.`,
-            likes: getRandomInt(15, 200),
-            comments: generateComments()
-        });
-    }
+  for (let i = 1; i <= 25; i++) {
+    photos.push({
+      id: i,
+      url: `photos/${i}.jpg`,
+      description: `Описание фотографии номер ${i}.`,
+      likes: getRandomInt(15, 200),
+      comments: generateComments()
+    });
+  }
 
-    return photos;
+  return photos;
 }
