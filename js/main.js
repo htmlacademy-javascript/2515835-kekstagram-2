9b90cc0a87423aefe0b11567f3424971708e506d
Tuvakova <tuvakovakamilla54@gmail.com>
2025-07-01 20:43:51 +0300

    Перламутровые пуговицы

--- js/main.js
+++ js/main.js
@@ -1,9 +1,7 @@
 import { renderPhotos } from './renderPhoto.js';
 import { openBigPicture } from './bigPicture.js';
 import { fetchPhotos, sendFormData } from './api.js';
-
-const form = document.querySelector('.img-filters__form');
-
+import { formModule } from './formModule.js';
 
 const showMessage = (templateId) => {
   const template = document.querySelector(`#${templateId}`);
@@ -17,50 +15,119 @@ const showMessage = (templateId) => {
   }, 5000);
 };
 
+const debounce = (func, delay) => {
+  let timeoutId;
+  return (...args) => {
+    if (timeoutId) clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => {
+      func(...args);
+    }, delay);
+  };
+};
+
+
 fetchPhotos()
   .then((arr) => {
-    console.log(arr);
+    let allPhotos = [...arr];
+
+
     renderPhotos(arr);
+
 
     document.querySelectorAll('.thumbnail').forEach((thumb, index) => {
       thumb.addEventListener('click', () => {
         openBigPicture(arr[index]);
       });
     });
+
+
+    const filtersContainer = document.querySelector('.img-filters');
+    if (filtersContainer) {
+      filtersContainer.classList.remove('img-filters--inactive');
+
+      const form = document.querySelector('.img-filters__form');
+
+      const getRandomInt = (min, max) => {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min +1)) + min;
+      };
+
+      const getRandomPhotos = (photos, count) => {
+        const shuffled = [...photos];
+        for (let i=shuffled.length-1; i>0; i--) {
+          const j=getRandomInt(0,i);
+          [shuffled[i], shuffled[j]]=[shuffled[j], shuffled[i]];
+        }
+        return shuffled.slice(0,count);
+      };
+
+      const clearPhotos = () => {
+        document.querySelector('.pictures').innerHTML='';
+      };
+
+      const applyFilter = (filterId) => {
+        let filteredPhotos=[];
+        switch(filterId){
+          case 'filter-default':
+            filteredPhotos=[...allPhotos];
+            break;
+          case 'filter-random':
+            filteredPhotos=getRandomPhotos(allPhotos,10);
+            break;
+          case 'filter-discussed':
+            filteredPhotos=[...allPhotos].sort((a,b)=>b.comments.length - a.comments.length);
+            break;
+          default:
+            filteredPhotos=[...allPhotos];
+        }
+        clearPhotos();
+        renderPhotos(filteredPhotos);
+      };
+
+      form.addEventListener('click', debounce((evt)=>{
+        if(evt.target.tagName!=='BUTTON') return;
+
+
+        form.querySelectorAll('button').forEach(btn=>btn.classList.remove('img-filters__button--active'));
+        evt.target.classList.add('img-filters__button--active');
+
+        const filterId=evt.target.id;
+        applyFilter(filterId);
+
+      },500));
+    }
+
+
+    if (form) {
+      form.addEventListener('submit', (evt)=>{
+        evt.preventDefault();
+        const formData=new FormData(form);
+        sendFormData(formData)
+          .then(()=>{
+            showMessage('success');
+            form.reset();
+          })
+          .catch(()=>{
+            showMessage('error');
+          });
+      });
+    }
+
+
+
+    const formValidationData={
+      hashtags:'',
+      comment:'Пример комментария',
+    };
+
+    formModule.addValidationRule('hashtags', formModule.rules.required('Хэштеги обязательны'));
+    formModule.addValidationRule('comment', formModule.rules.required('Комментарий обязателен'));
+
+    const errors= formModule.validate(formValidationData);
+    console.log(errors);
+
   })
   .catch(() => {
     showMessage('data-error');
-  });
-
-if (form) {
-  form.addEventListener('submit', (evt) => {
-    evt.preventDefault();
-
-    const formData = new FormData(form);
-
-    sendFormData(formData)
-      .then(() => {
-        showMessage('success');
-        form.reset();
-      })
-      .catch(() => {
-        showMessage('error');
-      });
-  });
-}
-
-
-
-
-import { formModule } from './formModule.js';
-const formData = {
-  hashtags: '',
-  comment: 'Пример комментария',
-};
-
-formModule.addValidationRule('hashtags', formModule.rules.required('Хэштеги обязательны'));
-formModule.addValidationRule('comment', formModule.rules.required('Комментарий обязателен'));
-
-
-const errors = formModule.validate(formData);
-console.log(errors);
+});
